0919e208b0e328b5b4a8e73389e4eda174ef87a2
david <hirakoshinji722@gmail.com>
2021-02-06 10:52:31 +0100

    add pre commit hook to run ktlint and spotless before committing.

--- buildSrc/src/main/kotlin/io/davidosemwota/microlytxtask/buildsource/utils/SystemOSUtils.kt
+++ buildSrc/src/main/kotlin/io/davidosemwota/microlytxtask/buildsource/utils/SystemOSUtils.kt
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2021   David Osemwota.
+ * Copyright (c) 17/11/2020 16:23   David Osemwota.
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -21,3 +21,17 @@
  * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
  * SOFTWARE.
  */
+
+package io.davidosemwota.microlytxtask.buildsource.utils
+
+import java.util.Locale
+
+/**
+ * Util to check if the project run on Linux or Mac operating system
+ *
+ * @return true if the operating system is one of them
+ */
+fun isLinuxOrMacOs(): Boolean {
+    val osName = System.getProperty("os.name").toLowerCase(Locale.ROOT)
+    return listOf("linux", "mac os", "macos").contains(osName)
+}
